ee653b83dd87159c6579893a2edcc0004a5669f0
Goh Sato <goh.sato@outlook.com>
2020-01-25 21:52:45 -0800

    Basic webserver

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,5 @@
 module extremeWorkload.com/daytrader
 
 go 1.13
+
+require github.com/gorilla/mux v1.7.3
--- src/web/web.go
+++ src/web/web.go
@@ -2,8 +2,72 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/mux"
 )
 
+func parseCommandRequest(r *http.Request) string {
+	vars := mux.Vars(r)
+	var command strings.Builder
+	command.WriteString(vars["cmd"])
+	r.ParseForm()
+	for _, v := range r.Form {
+		command.WriteString(", " + v[0])
+	}
+	return command.String()
+}
+
+// Creates a route method. Whenever the route is called, it always uses the same socket
+func createCommandRoute(transactionClient TransactionClient) func(w http.ResponseWriter, r *http.Request) {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		command := parseCommandRequest(r)
+		status, message, err := transactionClient.sendCommand(command)
+
+		w.WriteHeader(status)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		} else {
+			w.Write([]byte(message))
+		}
+
+	}
+
+}
+
+func heartbeat(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("pong"))
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func getRouter(transactionClient TransactionClient) http.Handler {
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	commandRoute := createCommandRoute(transactionClient)
+	myRouter.HandleFunc("/command/{cmd}", commandRoute)
+
+	myRouter.HandleFunc("/heartbeat", heartbeat)
+
+	myRouter.Use(loggingMiddleware)
+
+	return myRouter
+}
+
 func main() {
-	fmt.Println("Hello, world.")
+
+	transactionClient := TransactionClient{
+		Network:       "tcp",
+		RemoteAddress: ":8081",
+	}
+	transactionClient.ConnectSocket()
+	fmt.Println("start server")
+	http.ListenAndServe(":9090", getRouter(transactionClient))
 }
