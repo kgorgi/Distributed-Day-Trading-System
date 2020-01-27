74cb3bc9b679311585e7e176e5ef958bdacb7fcd
Goh Sato <goh.sato@outlook.com>
2020-01-27 14:00:19 -0800

    Add error handling

--- src/web/transactionClient.go
+++ src/web/transactionClient.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"extremeWorkload.com/daytrader/lib"
-	"fmt"
 	"net"
+
+	"extremeWorkload.com/daytrader/lib"
 )
 
 const transactionServerDockerAddress = "transaction-server:5000"
@@ -15,25 +15,21 @@ const transactionServerDockerAddress = "transaction-server:5000"
 type TransactionClient struct{}
 
 // SendCommand send command to transaction server
-func (transactionClient TransactionClient) SendCommand(command map[string]string) (int, string, error) {
+func (transactionClient *TransactionClient) SendCommand(command map[string]string) (int, string, error) {
 	var err error
 	commandJSON, err := json.Marshal(command)
-
 	if err != nil {
-		fmt.Println(err)
+		return lib.StatusSystemError, "", err
 	}
+
 	conn, err := net.Dial("tcp", transactionServerDockerAddress)
 	if err != nil {
-		fmt.Println(err)
+		return lib.StatusSystemError, "", err
 	}
 
-	status, message, err2 := lib.ClientSendRequest(conn, string(commandJSON))
+	status, message, err := lib.ClientSendRequest(conn, string(commandJSON))
 
 	conn.Close()
 
-	//handle reconnect here
-	if err2 != nil {
-		fmt.Println(err2)
-	}
 	return status, message, err
 }
