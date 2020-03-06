023cfd3e238dddee8c7f20780c35b2a440b45912
Goh Sato <goh.sato@outlook.com>
2020-03-05 16:08:25 -0800

    Update tests to read cert from env

--- src/lib/user/client.go
+++ src/lib/user/client.go
@@ -7,30 +7,26 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"fmt"
 )
 
-const webserverAddress = "https://localhost:8080/command/"
+const webserverAddress = "https://localhost:9090/command/"
 
-const caCert = `-----BEGIN CERTIFICATE-----
-MIIDCTCCAfGgAwIBAgIUJEv0GYYf5NgCkSt99YAlApZ/kgQwDQYJKoZIhvcNAQEL
-BQAwFDESMBAGA1UEAwwJbG9jYWxob3N0MB4XDTIwMDMwMTIxMjI0NVoXDTIxMDMw
-MTIxMjI0NVowFDESMBAGA1UEAwwJbG9jYWxob3N0MIIBIjANBgkqhkiG9w0BAQEF
-AAOCAQ8AMIIBCgKCAQEAvfYpuQ3SD9MqgxmidWSd6SYYqp5sUVtJJBjtMtdg2hp0
-lPys3mtMb2/fCWyDzkDew2Ks+TqGk2F4ueHavRZjSbWmJKhoBq1QCLbiIj30OW6O
-uYg4a3Ds3B6KS6MmYotkUHgUBYsQ01kK6ofKbMUc3aaLzCf+J6JVa+V6YbX6QVQn
-JxAZr2CU18rjIWQofPx0Rt5G/RzyEZx5dQWa7u5JXAvNn7vjOYSjve9xEYkt/jxr
-XD0Z5Vucccq0z8rDhj5HAbGRevXQT+S+KRKoiWAE97Brk+coxqfvZ+8a6ZW0jpYy
-phfnqaHfEw1uhmRSVzDkZrWSwdtcHN996Q7MpEOVQwIDAQABo1MwUTAdBgNVHQ4E
-FgQUQQT2f6J/Sb6T3RP5s/cB3zJQAH8wHwYDVR0jBBgwFoAUQQT2f6J/Sb6T3RP5
-s/cB3zJQAH8wDwYDVR0TAQH/BAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQEACeOn
-l9vOsQLnER625OgCPhoLyx8oKwdM+Dh4m2PJCt0vBJnFKCtRrQekd4ryyCSYgg/c
-zkQHSwRr4OCA64PiLKfRdNpdYnQ4TrROeL6c06IJ3IlnXtoFeRzYp8T2IlSMwt3L
-tFQKsqgie5aMDNtXjr83W9RUUSr/LGFqI3OOihyAWqye0zWSeUoqaFJC2aFBaov9
-0na8c3J4UNTFX2S6tlowIRe6RJxROIRWjg/SzdRBpstrImgaHR0DyGCUk7PF/gAO
-8An2umJWjNWHHr8aiQ3nlyTImlx6fc8RrA8JRbukD2UcNc4IBfPFYek9vO/li9/B
-zHiz81RZYkHq3ixFnQ==
------END CERTIFICATE-----
-`
+var caCertPool *x509.CertPool
+
+func initCertPool() error {
+	envCaCertLocation := os.Getenv("CLIENT_SSL_CERT_LOCATION")
+	fmt.Println(envCaCertLocation)
+	caCert, err := ioutil.ReadFile(envCaCertLocation)
+	if err != nil {
+		return err
+	}
+
+	caCertPool = x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	return nil
+}
 
 type commandParams struct {
 	UserID      string
@@ -56,9 +52,9 @@ func createParameters(command commandParams) url.Values {
 }
 
 func makeRequest(httpMethod string, command string, params url.Values) (int, string, error) {
-
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM([]byte(caCert))
+	if caCertPool == nil{
+		initCertPool()
+	}
 	// // Create a HTTPS client and supply the created CA pool and certificate
 	client := &http.Client{
 		Transport: &http.Transport{
