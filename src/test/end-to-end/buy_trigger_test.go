c6166d3aa480af3d24476da827e0ab4f7cacb661
Goh Sato <goh.sato@outlook.com>
2020-04-05 21:19:22 -0700

    Add sell trigger tests

--- src/test/end-to-end/buy_trigger_test.go
+++ src/test/end-to-end/buy_trigger_test.go
@@ -8,14 +8,24 @@ import (
 )
 
 func setupBuyTriggerTest(t *testing.T) {
-	status, body, err := userClient.CancelSetBuyRequest(userid, stockSymbol)
+	status, body, err := userClient.AddRequest(userid, lib.CentsToDollars(addAmount))
+	handleErrors("Add failed", status, body, err, t)
+
+	status, body, err = userClient.CancelSetBuyRequest(userid, stockSymbol)
 	checkSystemError("Cancel Buy failed", status, body, err, t)
 	summary := getUserSummary(userClient, userid, t)
 	if getTestStockTrigger(summary, false) != nil {
 		t.Error("Trigger was not cleared initially")
 	}
-	status, body, err = userClient.AddRequest(userid, lib.CentsToDollars(addAmount))
-	handleErrors("Add failed", status, body, err, t)
+}
+
+func TestTriggerBuyNotEnough(t *testing.T) {
+	setupBuyTriggerTest(t)
+	summaryBefore := getUserSummary(userClient, userid, t)
+
+	status, body, err := userClient.SetBuyTriggerRequest(userid, stockSymbol, lib.CentsToDollars(summaryBefore.Cents+10))
+	checkUserCommandError("Should have failed for not having enought amount", status, body, err, t)
+
 }
 
 func TestTriggerBuy(t *testing.T) {
@@ -24,10 +34,14 @@ func TestTriggerBuy(t *testing.T) {
 
 	summaryBefore := getUserSummary(userClient, userid, t)
 
-	status, body, err := userClient.SetBuyAmountRequest(userid, stockSymbol, lib.CentsToDollars(buyAmount))
+	status, body, err := userClient.SetBuyTriggerRequest(userid, stockSymbol, lib.CentsToDollars(buyTriggerPrice))
+	checkUserCommandError("Should have failed for not having amoutn set", status, body, err, t)
+
+	// + 1 is to make sure leftover reserve money is returned
+	status, body, err = userClient.SetBuyAmountRequest(userid, stockSymbol, lib.CentsToDollars(buyAmount+1))
 	handleErrors("Set Buy Amount failed", status, body, err, t)
 
-	status, body, _ = userClient.SetBuyTriggerRequest(userid, stockSymbol, lib.CentsToDollars(buyTriggerPrice))
+	status, body, err = userClient.SetBuyTriggerRequest(userid, stockSymbol, lib.CentsToDollars(buyTriggerPrice))
 	handleErrors("Set Buy Trigger failed", status, body, err, t)
 
 	summaryAfter := getUserSummary(userClient, userid, t)
@@ -84,6 +98,33 @@ func TestTriggerBuyEditValues(t *testing.T) {
 
 	isEqual(triggerAfter.Amount_Cents, buyAmount*2, "Amount was not updated", t)
 	isEqual(triggerAfter.Price_Cents, buyTriggerPrice*2, "Triggerprice was not updated", t)
+
+	status, body, err = userClient.CancelSetBuyRequest(userid, stockSymbol)
+	checkSystemError("Cancel Buy failed", status, body, err, t)
+}
+
+func TestTriggerBuyCancel(t *testing.T) {
+	setupBuyTriggerTest(t)
+
+	status, body, err := userClient.CancelSetBuyRequest(userid, stockSymbol)
+	checkUserCommandError("Expected a user error response", status, body, err, t)
+
+	summaryBefore := getUserSummary(userClient, userid, t)
+
+	status, body, err = userClient.SetBuyAmountRequest(userid, stockSymbol, lib.CentsToDollars(buyAmount))
+	handleErrors("Set Buy Amount failed", status, body, err, t)
+
+	status, body, _ = userClient.SetBuyTriggerRequest(userid, stockSymbol, lib.CentsToDollars(buyTriggerPrice))
+	handleErrors("Set Buy Trigger failed", status, body, err, t)
+
+	summaryAfter := getUserSummary(userClient, userid, t)
+	isEqual(summaryAfter.Cents, summaryBefore.Cents-buyAmount, "Money was not witheld", t)
+
+	status, body, err = userClient.CancelSetBuyRequest(userid, stockSymbol)
+	handleErrors("Cancel Buy failed", status, body, err, t)
+
+	summaryAfter = getUserSummary(userClient, userid, t)
+	isEqual(summaryAfter.Cents, summaryBefore.Cents, "Money was not returned", t)
 
 	status, body, err = userClient.CancelSetBuyRequest(userid, stockSymbol)
 	checkSystemError("Cancel Buy failed", status, body, err, t)
